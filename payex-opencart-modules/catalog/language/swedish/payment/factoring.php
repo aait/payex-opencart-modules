67b66354de4fe7ffe9c03561fc5678d72efced2b
Jonathan <jonathan@aait.se>
2014-12-03 12:30:47 +0100

    adding opencart module

---
+++ payex-opencart-modules/catalog/language/swedish/payment/factoring.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('DIR_APPLICATION')) {
+    die();
+}
+
+
+$_['text_title'] = 'PayEx Factoring';
+$_['text_description'] = 'Var god och skriv in ditt personnummer och bekräfta ordern.';
+$_['text_social_security_number'] = 'Personnummer:';
+$_['text_select_payment_method'] = 'Please select payment method:';
+$_['text_factoring'] = 'Invoice 2.0 (Factoring)';
+$_['text_part_payment'] = 'Part Payment';
